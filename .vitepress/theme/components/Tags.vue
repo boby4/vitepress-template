c97823137d51adbbd8bdd9b32f0e88df8cd55bfd
boby4 <1924117768@qq.com>
2023-08-31 10:50:35 +0800

    监听url变化调试

--- .vitepress/theme/components/Tags.vue
+++ .vitepress/theme/components/Tags.vue
@@ -1,90 +1,105 @@
 <template>
-    <div class="tags">
-        <span @click="toggleTag(key)" v-for="(item, key) in data" :class="key == selectTag ? 'tag_select' : 'tag'">
-            # {{ key }} <strong>{{ data[key].length }}</strong>
-        </span>
+  <div class="tags">
+    <span
+      @click="toggleTag(key)"
+      v-for="(item, key) in data"
+      :class="key == selectTag ? 'tag_select' : 'tag'"
+    >
+      # {{ key }} <strong>{{ data[key].length }}</strong>
+    </span>
+  </div>
+  <div class="tag-header">{{ selectTag }}</div>
+  <a
+    :href="withBase(article.regularPath)"
+    v-for="(article, index) in data[selectTag]"
+    :key="index"
+    class="posts"
+  >
+    <div class="post-container">
+      <div class="post-dot"></div>
+      {{ article.frontMatter.title }}
     </div>
-    <div class="tag-header">{{ selectTag }}</div>
-    <a :href="withBase(article.regularPath)" v-for="(article, index) in data[selectTag]" :key="index" class="posts">
-        <div class="post-container">
-            <div class="post-dot"></div>
-            {{ article.frontMatter.title }}
-        </div>
-        <div class="date">{{ article.frontMatter.date }}</div>
-    </a>
+    <div class="date">{{ article.frontMatter.date }}</div>
+  </a>
 </template>
 <script setup>
 import { computed, ref, watch } from 'vue'
 import { useData, withBase } from 'vitepress'
 import { initTags } from '../../utils/functions'
-let url = window.location?.href.split('?')[1]
-console.log('location变化', window.location)
+// let url = window.location?.href.split('?')[1]
+let url = ref(window.location.search)
+console.log('location变化', url)
 let params = new URLSearchParams(url)
 const { theme } = useData()
 const data = computed(() => initTags(theme.value.posts))
 let selectTag = ref(params.get('tag') ? params.get('tag') : '')
 const toggleTag = (tag) => {
-    selectTag.value = tag
+  selectTag.value = tag
 }
-if(url) toggleTag(params.get('tag'))
+watch(url, (newUrl) => {
+  params = new URLSearchParams(newUrl)
+  if (params.has('tag')) {
+    toggleTag(params.get('tag'))
+  }
+})
 </script>
 
 <style scoped>
 .tags {
-    margin-top: 14px;
-    display: flex;
-    flex-wrap: wrap;
+  margin-top: 14px;
+  display: flex;
+  flex-wrap: wrap;
 }
 .tag {
-    display: inline-block;
-    padding: 4px 16px;
-    margin: 6px 8px;
-    font-size: 0.7rem;
-    line-height: 25px;
-    font-weight: 600;
-    border-radius: 2px;
-    background-color: #EFEFEF;
-    transition: 0.4s;
-    border-radius: 4px;
-    color: var(--vp-c-text-1);
-    cursor: pointer;
+  display: inline-block;
+  padding: 4px 16px;
+  margin: 6px 8px;
+  font-size: 0.7rem;
+  line-height: 25px;
+  font-weight: 600;
+  border-radius: 2px;
+  background-color: #efefef;
+  transition: 0.4s;
+  border-radius: 4px;
+  color: var(--vp-c-text-1);
+  cursor: pointer;
 }
-.tag:hover{
-    background-color: var(--vp-c-brand);
-    transition: 0.4s;
-    border-radius: 4px;
-    color: rgb(255, 255, 255);
+.tag:hover {
+  background-color: var(--vp-c-brand);
+  transition: 0.4s;
+  border-radius: 4px;
+  color: rgb(255, 255, 255);
 }
 .tag_select {
-    display: inline-block;
-    padding: 4px 16px;
-    margin: 6px 8px;
-    border-radius: 2px;
-    font-size: 0.7rem;
-    font-weight: 600;
-    line-height: 25px;
-    background-color: var(--vp-c-brand);
-    transition: 0.4s;
-    border-radius: 4px;
-    color: white;
-    cursor: pointer;
+  display: inline-block;
+  padding: 4px 16px;
+  margin: 6px 8px;
+  border-radius: 2px;
+  font-size: 0.7rem;
+  font-weight: 600;
+  line-height: 25px;
+  background-color: var(--vp-c-brand);
+  transition: 0.4s;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
 }
 .tag strong {
-    color: var(--vp-c-brand);
+  color: var(--vp-c-brand);
 }
 .tag-header {
-    font-size: 1rem;
-    font-weight: 600;
-    margin: 2rem 0 1rem 0;
-    text-align: left;
+  font-size: 1rem;
+  font-weight: 600;
+  margin: 2rem 0 1rem 0;
+  text-align: left;
 }
 
 @media screen and (max-width: 768px) {
-    .tag-header {
-        font-size: 1rem;
-    }
-    .date {
-        font-size: 0.7rem;
-    }
+  .tag-header {
+    font-size: 1rem;
+  }
+  .date {
+    font-size: 0.7rem;
+  }
 }
 </style>
