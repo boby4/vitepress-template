8f3cbdb191623693335bfddf4d732b2bc73503dd
boby4 <1924117768@qq.com>
2023-09-21 11:57:45 +0800

    网站运行时间优化

--- .vitepress/theme/components/Copyright.vue
+++ .vitepress/theme/components/Copyright.vue
@@ -1,23 +1,30 @@
 <template>
-    <div class="site-footer">
-        MIT Licensed | Copyright © 2023-2024 <a class="vitepress" :href="website">{{ webTitle }}</a><br />
-        Powered by <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/">VitePress - 1.0.0-rc.47</a>
-        <p class="htmer_time">本站已实际运行 <strong>{{currentTimeHtml}}</strong></p>
-    </div>
+  <div class="site-footer">
+    MIT Licensed | Copyright © 2023-2024
+    <a class="vitepress" :href="website">{{ webTitle }}</a
+    ><br />
+    Powered by
+    <a class="vitepress" target="_blank" href="//vitepress.vuejs.org/"
+      >VitePress - 1.0.0-rc.47</a
+    >
+    <p class="htmer_time">
+      本站已实际运行 <strong>{{ currentTimeHtml }}</strong>
+    </p>
+  </div>
 </template>
 <script lang="ts" setup>
-import { calculateUptime  } from '../../utils/runTime'
+import { calculateUptime } from '../../utils/runTime'
 import { useData } from 'vitepress'
 import { onMounted, ref } from 'vue'
 
 onMounted(() => {
-    setTime(); // 初始化
-    setInterval(setTime, 1000); // 每秒钟刷新
-});
-let currentTimeHtml = ref('');
+  setTime() // 初始化
+  setInterval(setTime, 1000) // 每秒钟刷新
+})
+let currentTimeHtml = ref('')
 const setTime = () => {
-    const currentTime = calculateUptime(); // 调用公用方法
-    currentTimeHtml.value = currentTime
+  const currentTime = calculateUptime() // 调用公用方法
+  currentTimeHtml.value = currentTime
 }
 
 const { site, theme } = useData()
@@ -27,19 +34,19 @@ const webTitle = site.value.title
 
 <style>
 .site-footer {
-    color: #888;
-    text-align: center;
-    font-size: 0.75rem;
-    width: 100%;
-    padding: 10px 0 10px 0;
-    /* height: 4rem;
+  color: #888;
+  text-align: center;
+  font-size: 0.75rem;
+  width: 100%;
+  padding: 10px 0 10px 0;
+  /* height: 4rem;
     background: white;
     position: fixed;
     bottom: 0;
     z-index: 9; */
 }
 .vitepress {
-    color: var(--vp-c-text-1);
-    font-weight: 700;
+  color: var(--vp-c-text-1);
+  font-weight: 700;
 }
 </style>
--- .vitepress/utils/runTime.js
+++ .vitepress/utils/runTime.js
@@ -1,46 +1,18 @@
-export function secondToDate(second) {
-	if (!second) {
-		return [0, 0, 0, 0, 0];
-	}
-	var time = new Array(0, 0, 0, 0, 0);
-	if (second >= 365 * 24 * 3600) {
-		time[0] = parseInt(second / (365 * 24 * 3600));
-		second %= 365 * 24 * 3600;
-	}
-	if (second >= 24 * 3600) {
-		time[1] = parseInt(second / (24 * 3600));
-		second %= 24 * 3600;
-	}
-	if (second >= 3600) {
-		time[2] = parseInt(second / 3600);
-		second %= 3600;
-	}
-	if (second >= 60) {
-		time[3] = parseInt(second / 60);
-		second %= 60;
-	}
-	if (second > 0) {
-		time[4] = second;
-	}
-	return time;
-}
-
 export function calculateUptime() {
-	let create_time = Math.round(
-		new Date(Date.UTC(2023, 7, 15, 0, 0, 0)).getTime() / 1000
-	); // 注意月份是从0开始计数，所以月份要-1
-	let timestamp = Math.round(new Date().getTime() / 1000); // 不需要再加上时区偏移
-	let currentTime = secondToDate(timestamp - create_time);
-	let currentTimeHtml =
-		currentTime[0] +
-		'年' +
-		currentTime[1] +
-		'天' +
-		currentTime[2] +
-		'时' +
-		currentTime[3] +
-		'分' +
-		currentTime[4] +
-		'秒';
-	return currentTimeHtml;
+  let create_time = new Date(2023, 7, 15); // 月份是从 0 开始计数，所以 8 月对应 7
+  let currentTime = new Date();
+  let timeDiff = currentTime - create_time;
+  // 计算年、月、日、小时、分钟和秒
+  let years = Math.floor(timeDiff / (365 * 24 * 60 * 60 * 1000));
+  timeDiff -= years * 365 * 24 * 60 * 60 * 1000;
+  let months = Math.floor(timeDiff / (30 * 24 * 60 * 60 * 1000)); // 平均每月 30 天
+  timeDiff -= months * 30 * 24 * 60 * 60 * 1000;
+  let days = Math.floor(timeDiff / (24 * 60 * 60 * 1000));
+  timeDiff -= days * 24 * 60 * 60 * 1000;
+  let hours = Math.floor(timeDiff / (60 * 60 * 1000));
+  timeDiff -= hours * 60 * 60 * 1000;
+  let minutes = Math.floor(timeDiff / (60 * 1000));
+  timeDiff -= minutes * 60 * 1000;
+  let seconds = Math.floor(timeDiff / 1000);
+  return `${years}年${months}月${days}天${hours}小时${minutes}分钟${seconds}秒`;
 }
