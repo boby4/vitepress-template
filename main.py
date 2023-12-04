6e32661022ea27f2af783248f2bcfbf1db513590
boby4 <1924117768@qq.com>
2023-12-04 11:54:33 +0800

    zhushidi

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ birthdaym = os.environ["BIRTHDAY_MING"].split(',')
 
 
 # 获取天气和温度
-def get_weather(city):
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-    res = requests.get(url).json()
-    weather = res['data']['list'][0]
-    return weather['weather'], math.floor(weather['temp'])
+# def get_weather(city):
+#     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+#     res = requests.get(url).json()
+#     weather = res['data']['list'][0]
+#     return weather['weather'], math.floor(weather['temp'])
 
 
 # 当前城市、日期
